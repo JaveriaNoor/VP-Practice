3d42676bbfe1492ac5d1c3a6063f417c32235904
Javeria Noor <javerianoor67@gmail.com>
2016-04-03 08:25:51 -0700

    Added files via upload

---
+++ BuubleSort/BuubleSort/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuubleSort
+{
+    class Program
+    {
+        public class FindingSortedArray
+        {
+            static int[] IntegerArray = { 99,67,-1,4,45,32,1,2,3,44,0,-44,33,100,99,44,2,44,45 };
+            int loopLimit = IntegerArray.Length - 1;
+
+            public int [] numericArray()
+            {
+                for (int j = 0; j < IntegerArray.Length - 1;j++ )
+                {
+                    for (int i = 0; i < loopLimit; i++)
+                    {
+                        int temp = 0;
+                        if (IntegerArray[i] > IntegerArray[i + 1])
+                        {
+                            temp = IntegerArray[i + 1];
+                            IntegerArray[i + 1] = IntegerArray[i];
+                            IntegerArray[i] = temp;
+                        }
+                    }
+                loopLimit--;
+            }
+                for (int j = 0; j < IntegerArray.Length;j++ )
+                {
+                    Console.WriteLine(IntegerArray[j]);
+                }
+            return IntegerArray;
+            }
+        }
+        static void Main(string[] args)
+        {
+            FindingSortedArray obj = new FindingSortedArray();
+            obj.numericArray();
+            
+        }
+
+    }
+}
